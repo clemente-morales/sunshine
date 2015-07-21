c104ee1dbca070feae30cb8ad3d0763effdc775b
Clemente Morales Fernández <morales.fernandez.clemente@gmail.com>
2015-07-21 12:48:57 -0500

    Sunshine feature Data consume from WeatherApi

--- app/src/main/java/lania/edu/mx/sunshine/MainActivityFragment.java
+++ app/src/main/java/lania/edu/mx/sunshine/MainActivityFragment.java
@@ -3,12 +3,19 @@ package lania.edu.mx.sunshine;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,6 +23,8 @@ import java.util.List;
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private static final String TAG = MainActivityFragment.class.getSimpleName();
 
     public MainActivityFragment() {
     }
@@ -28,12 +37,65 @@ public class MainActivityFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_main, container, false);
 
-         adapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, getFakeData());
+        adapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, getRealData());
 
         ListView listView = (ListView) view.findViewById(R.id.listview_forecast);
         listView.setAdapter(adapter);
 
         return view;
+    }
+
+    private List<String> getRealData() {
+        HttpURLConnection connection = null;
+        BufferedReader reader = null;
+        String forecastJson = "";
+
+        try {
+            URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=06300&mode=json&units=metric&cnt=7");
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+
+
+            InputStream inputStream = connection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+            if (inputStream == null)
+                return null;
+
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line = reader.readLine();
+
+            while (line!=null) {
+                buffer.append(line);
+                buffer.append("\n");
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+            }
+
+            if (buffer.length()==0)
+                return  null;
+
+            forecastJson = buffer.toString();
+            Log.d(TAG, forecastJson);
+
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage(), e);
+        } finally {
+            if (connection!=null) {
+                connection.disconnect();
+            }
+
+            if (reader!=null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    Log.e(TAG, e.getMessage(), e);
+                }
+            }
+        }
+
+
+        return null;
+
     }
 
     @NonNull
